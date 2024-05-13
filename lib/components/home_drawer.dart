5b7b2b1d8a11ec9afc4cd4ed7857539034e322f6
kylabearneza <124786899+kylabearneza@users.noreply.github.com>
2024-05-13 21:06:04 +0800

    add result page

--- lib/components/home_drawer.dart
+++ lib/components/home_drawer.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/material.dart';
 import 'package:cc206_cictscape/components/chronoroom_drawer.dart';
 import 'package:cc206_cictscape/screens/welcomescreen.dart';
 import 'package:cc206_cictscape/components/calebot_drawer.dart';
+
+import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -23,7 +24,7 @@ class _HomeScreenState extends State<HomeScreen> {
             '',
             style: TextStyle(color: Colors.white),
           ),
-          backgroundColor:  const Color(0xFFFFF96B),
+          backgroundColor: const Color(0xFFFFF96B),
           actions: [
             IconButton(
               icon: const Icon(
@@ -154,80 +155,154 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Container(
               margin: const EdgeInsets.only(top: 120),
               decoration: const BoxDecoration(
-                color: Color(0xFFFFF96B),
+                color: Color(0xFFF1F2F6),
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(30),
                   topRight: Radius.circular(30),
                 ),
               ),
              child: LayoutBuilder(
-  builder: (context, constraints) {
+    builder: (context, constraints) {
     return SingleChildScrollView(
       child: ConstrainedBox(
         constraints: BoxConstraints(minHeight: constraints.maxHeight),
         child: IntrinsicHeight(
           child: Column(
-                children: [
-                SizedBox(height: 20),
-                ItemWidget2(
-                  imageUrl: 'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*',
-                  headerText: 'Lost Baxter',
-                  subtitleText: 'Bito-on,Jaro',
-                  additionalText: 'September 26,2023',
-                ),
-                SizedBox(height: 20),
-                ItemWidget2(
-                  imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAthAnl2GfAOsAUDlS6HbYed-XcujPch2sP5sjLqw05w&s',
-                  headerText: 'Lost Lila',
-                  subtitleText: 'Bito-on,Jaro',
-                  additionalText: 'September 26,2023',
-                ),
-                SizedBox(height: 20),
-                ItemWidget2(
-                  imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQb2u39PJUvqfWTorlSxcv1Iq2qnuATM63k64YtO5vPow&s',
-                  headerText: 'Found Puppy',
-                  subtitleText: 'Bito-on,Jaro',
-                  additionalText: 'September 26,2023',
-                ),
-                SizedBox(height: 20),
-                ItemWidget2(
-                  imageUrl: 'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*',
-                  headerText: 'Found Puppy',
-                  subtitleText: 'Bito-on,Jaro',
-                  additionalText: 'September 26,2023',
-                ),
-                SizedBox(height: 20),
-                ItemWidget2(
-                  imageUrl: 'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*',
-                  headerText: 'Found Puppy',
-                  subtitleText: 'Bito-on,Jaro',
-                  additionalText: 'September 26,2023',
-                ),
-                SizedBox(height: 20),
-                ItemWidget2(
-                  imageUrl: 'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*',
-                   headerText: 'Found Puppy',
-                  subtitleText: 'Bito-on,Jaro',
-                  additionalText: 'September 26,2023',
-                ),
-                SizedBox(height: 20),
-              ],
-              ),
-
-                      ),
-                    ),
+            children: [
+              SizedBox(height: 20),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ResultPage(
+                      imageUrl: 'https://static.vecteezy.com/system/resources/thumbnails/005/857/332/small_2x/funny-portrait-of-cute-corgi-dog-outdoors-free-photo.jpg',
+                      headerText: 'Cute Corgi Dog',
+                      subtitleText: '4-5 kgs',
+                      additionalText: 'Brown',
+                      dateLost: 'April 25, 2023',
+                      location: 'Central Park',
+                      breed: 'Corgi',
+                      sex: 'Male',
+                    )),
                   );
                 },
+                child: ItemWidget2(
+                  imageUrl: 'https://static.vecteezy.com/system/resources/thumbnails/005/857/332/small_2x/funny-portrait-of-cute-corgi-dog-outdoors-free-photo.jpg',
+                  headerText: 'Cute Corgi Dog',
+                      subtitleText: '4-5 kgs',
+                      additionalText: 'Brown',
+                )
               ),
-
+              SizedBox(height: 20),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ResultPage(
+                       imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpOjNUJrdthHEKKNnqqBlAmXyK2J4XoeAzaJYOAKI-HT7vaJaFrFg9Zafkfz5-q6vKXUQ&usqp=CAU',
+                      headerText: 'RSPCA Dogs',
+                      subtitleText: '4-5 kgs',
+                      additionalText: 'White Brown',
+                      dateLost: 'April 25, 2023',
+                      location: 'Central Park',
+                      breed: 'Corgi',
+                      sex: 'Male',
+                    )),
+                  );
+                },
+                child: ItemWidget2(
+                  imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpOjNUJrdthHEKKNnqqBlAmXyK2J4XoeAzaJYOAKI-HT7vaJaFrFg9Zafkfz5-q6vKXUQ&usqp=CAU',
+                  headerText: 'RSPCA Dogs',
+                      subtitleText: '4-5 kgs',
+                      additionalText: 'White Brown',
+                ),
+              ),
+              SizedBox(height: 20),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ResultPage(
+                      imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAthAnl2GfAOsAUDlS6HbYed-XcujPch2sP5sjLqw05w&s',
+                      headerText: 'Lost Baxter',
+                      subtitleText: '4-5 kgs',
+                      additionalText: ' Brown',
+                      dateLost: 'April 25, 2023',
+                      location: 'Central Park',
+                      breed: 'Corgi',
+                      sex: 'Male',
+                    )),
+                  );
+                },
+                child: ItemWidget2(
+                  imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAthAnl2GfAOsAUDlS6HbYed-XcujPch2sP5sjLqw05w&s',
+                    headerText: 'Lost Baxter',
+                      subtitleText: '4-5 kgs',
+                      additionalText: ' Brown',
+                ),
+              ),
+              SizedBox(height: 20),
+               SizedBox(height: 20),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ResultPage(
+                     imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpOjNUJrdthHEKKNnqqBlAmXyK2J4XoeAzaJYOAKI-HT7vaJaFrFg9Zafkfz5-q6vKXUQ&usqp=CAU',
+                      headerText: 'RSPCA Dogs',
+                      subtitleText: '4-5 kgs',
+                      additionalText: 'White Brown',
+                      dateLost: 'April 25, 2023',
+                      location: 'Central Park',
+                      breed: 'Corgi',
+                      sex: 'Male',
+                    )),
+                  );
+                },
+                child: ItemWidget2(
+                  imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpOjNUJrdthHEKKNnqqBlAmXyK2J4XoeAzaJYOAKI-HT7vaJaFrFg9Zafkfz5-q6vKXUQ&usqp=CAU',
+                  headerText: 'RSPCA Dogs',
+                      subtitleText: '4-5 kgs',
+                      additionalText: 'White Brown',
+                ),
+              ),
+                SizedBox(height: 20),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ResultPage(
+                      imageUrl: 'https://static.vecteezy.com/system/resources/thumbnails/005/857/332/small_2x/funny-portrait-of-cute-corgi-dog-outdoors-free-photo.jpg',
+                      headerText: 'Cute Corgi Dog',
+                      subtitleText: '4-5 kgs',
+                      additionalText: 'White Brown',
+                      dateLost: 'April 25, 2023',
+                      location: 'Central Park',
+                      breed: 'Corgi',
+                      sex: 'Male',
+                    )),
+                  );
+                },
+                child: ItemWidget2(
+                  imageUrl: 'https://static.vecteezy.com/system/resources/thumbnails/005/857/332/small_2x/funny-portrait-of-cute-corgi-dog-outdoors-free-photo.jpg',
+                  headerText: 'Cute Corgi Dog',
+                      subtitleText: '4-5 kgs',
+                      additionalText: 'White Brown',
+                )
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  },
+),
             ),
           ),
         ],
       ),
     );
   }
-
-  
 
   Widget _buildHoverableDrawerItem(
       String title, IconData icon, VoidCallback onTap) {
@@ -255,17 +330,6 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {
       _selectedIndex = index;
     });
-  }
-}
-
-class ItemWidget extends StatelessWidget {
-  final String text;
-
-  const ItemWidget({Key? key, required this.text}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(text);
   }
 }
 
@@ -317,9 +381,12 @@ class ItemWidget2 extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 SizedBox(height: 50), // Add vertical padding
-                Text(
-                  headerText, // Header text
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+                SizedBox(
+                  width: double.infinity,
+                  child: Text(
+                    headerText, // Header text
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+                  ),
                 ),
                 SizedBox(height: 5), // Add some vertical spacing
                 Text(
@@ -334,6 +401,210 @@ class ItemWidget2 extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ResultPage extends StatefulWidget {
+  final String imageUrl;
+  final String headerText;
+  final String subtitleText;
+  final String additionalText;
+  final String dateLost;
+  final String location;
+  final String breed;
+  final String sex;
+
+  const ResultPage({
+    required this.imageUrl,
+    required this.headerText,
+    required this.subtitleText,
+    required this.additionalText,
+    required this.dateLost,
+    required this.location,
+    required this.breed,
+    required this.sex,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _ResultPageState createState() => _ResultPageState();
+}
+
+class _ResultPageState extends State<ResultPage> {
+  bool isCallHovered = false;
+  bool isMarkAsFoundHovered = false;
+
+ void _callOwner() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(
+            'Calling...',
+            textAlign: TextAlign.center,),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.network(
+                'https://scontent.xx.fbcdn.net/v/t1.15752-9/441083894_416066551184552_3073040650696970110_n.png?stp=dst-png_p206x206&_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEfyWiwY2nsaVx-cgDMeNrZo5TAuuCnWoKjlMC64KdagnnNB4A5UzSYH3RKCrbAyWmql19lthjMbWQNEhkeZQW_&_nc_ohc=4L8DGPa14nEQ7kNvgFTyH8-&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_Q7cD1QGBIM4Tb0xkmAlRysJsSS7fbkM77KgjC9bNeTL-fqjzFw&oe=666968C3',
+                height: 200,
+                width: 200,
+              ),
+               IconButton(
+              onPressed: () {
+                      Navigator.of(context).pop();
+              },
+              icon: Icon(Icons.call_end, color: Colors.red),
+              iconSize: 40,
+            ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Result')),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.network(
+              widget.imageUrl,
+              fit: BoxFit.cover,
+              height: 300,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.headerText,
+                        style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        widget.subtitleText,
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Text(
+                        widget.additionalText,
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      SizedBox(height: 20),
+                      Divider(),
+                      SizedBox(height: 10),
+                      Text(
+                        'Date Lost: ${widget.dateLost}',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Location: ${widget.location}',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Breed: ${widget.breed}',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Sex: ${widget.sex}',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 20),
+                      Divider(),
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: MouseRegion(
+                              onEnter: (_) {
+                                setState(() {
+                                  isCallHovered = true;
+                                });
+                              },
+                              onExit: (_) {
+                                setState(() {
+                                  isCallHovered = false;
+                                });
+                              },
+                             child: ElevatedButton(
+                                onPressed: _callOwner,
+                                style: ElevatedButton.styleFrom(
+                                  foregroundColor: isCallHovered ? Colors.white : Color(0xFF121212), backgroundColor: isCallHovered ? Color(0xFF121212) : Color(0xFFF6F6F6),
+                                  padding: EdgeInsets.symmetric(vertical: 15),
+                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                ),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Icon(Icons.call, color: isCallHovered ? Colors.white : Color(0xFF121212)),
+                                    SizedBox(width: 10),
+                                    Text('Call', style: TextStyle(color: isCallHovered ? Colors.white : Color(0xFF121212))),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 20),
+                          Expanded(
+                            child: MouseRegion(
+                              onEnter: (_) {
+                                setState(() {
+                                  isMarkAsFoundHovered = true;
+                                });
+                              },
+                              onExit: (_) {
+                                setState(() {
+                                  isMarkAsFoundHovered = false;
+                                });
+                              },
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  // Mark as found button functionality
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  foregroundColor: isMarkAsFoundHovered ? Colors.white : Color(0xFF121212), backgroundColor: isMarkAsFoundHovered ? Color(0xFF121212) : Color(0xFFF6F6F6),
+                                  padding: EdgeInsets.symmetric(vertical: 15),
+                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                ),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Icon(Icons.done, color: isMarkAsFoundHovered ? Colors.white : Color(0xFF121212)),
+                                    SizedBox(width: 10),
+                                    Text('Mark as Found', style: TextStyle(color: isMarkAsFoundHovered ? Colors.white : Color(0xFF121212))),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
